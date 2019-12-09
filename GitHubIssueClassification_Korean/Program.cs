4d91735223a24a446245597e3263e49a2cc44024
Hongsuk Kim <hskim618@hotmail.com>
2019-12-09 21:44:01 +0900

    Init

---
+++ GitHubIssueClassification_Korean/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+
+namespace GitHubIssueClassification
+{
+    class Program
+    {
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string _trainDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "taeyo_train.tsv");
+        private static string _testDataPath => Path.Combine(_appPath, "..", "..", "..", "Data", "taeyo_test.tsv");
+        private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
+
+        private static MLContext _mlContext;
+        private static PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+        private static ITransformer _trainedModel;
+        static IDataView _trainingDataView;
+
+        static void Main(string[] args)
+        {
+            _mlContext = new MLContext(seed: 0);
+
+            // Prepare Data
+            _trainingDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue>(_trainDataPath, hasHeader: true);
+
+            // Prepare Pipeline
+            var pipeline = _mlContext.Transforms.Conversion.MapValueToKey(inputColumnName: "Area", outputColumnName: "Label")
+                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Title", outputColumnName: "TitleFeaturized"))
+                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Description", outputColumnName: "DescriptionFeaturized"))
+                .Append(_mlContext.Transforms.Concatenate("Features", "TitleFeaturized", "DescriptionFeaturized"))
+                .AppendCacheCheckpoint(_mlContext)
+                .Append(_mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features", maximumNumberOfIterations: 1000))
+                .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            // Training Model
+            _trainedModel = pipeline.Fit(_trainingDataView);
+
+            // Single Prediction
+            _predEngine = _mlContext.Model.CreatePredictionEngine<GitHubIssue, IssuePrediction>(_trainedModel);
+
+            GitHubIssue issue = new GitHubIssue()
+            {
+                Title = "WebSockets communication is slow in my machine",
+                Description = "The WebSockets communication used under the covers by SignalR looks like is going slow in my development machine.."
+            };
+
+            var prediction = _predEngine.Predict(issue);
+            Console.WriteLine($"=============== Single Prediction just-trained-model - Result: {prediction.Area}===============");
+
+            // Evaluate
+            var testDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue>(_testDataPath, hasHeader: true);
+            var testMetrics = _mlContext.MulticlassClassification.Evaluate(_trainedModel.Transform(testDataView));
+
+            Console.WriteLine($"*************************************************************************************************************");
+            Console.WriteLine($"* Metrics for Multi-class Classification model - Test Data ");
+            Console.WriteLine($"*----------------------------------------------------------------------------------------------------------- - ");
+            Console.WriteLine($"* MicroAccuracy: {testMetrics.MicroAccuracy:0.###}");
+            Console.WriteLine($"* MacroAccuracy: {testMetrics.MacroAccuracy:0.###}");
+            Console.WriteLine($"* LogLoss: {testMetrics.LogLoss:#.###}");
+            Console.WriteLine($"* LogLossReduction: {testMetrics.LogLossReduction:#.###}");
+            Console.WriteLine($"*************************************************************************************************************");
+
+            // Save Model
+            _mlContext.Model.Save(_trainedModel, _trainingDataView.Schema, _modelPath);
+
+            // Predict Issue
+            ITransformer loadedModel = _mlContext.Model.Load(_modelPath, out var modelInputSchema);
+
+            GitHubIssue singleIssue = new GitHubIssue()
+            {
+                //Title = "socket 중계 서버..",
+                //Description = "오랜만에 질문 올립니다.  tcp socket 중계 서버를 만들어야 하는데요..  간단하게 얘기하면 n개의 장비가 있고 n개의 컨트롤 시스템이 존재 각 장비는 지정 컨트롤 시스템과 정보 송/수신. 말하자면 1:1의 양방향 통신이 n개가 관리 가능해야 한다는 얘긴데 .  socket.poll, threadPool.. 등등... 뭘 어찌 구현할지 좀 감이 안오네요. 비슷한 구현 해보신 분 힌트좀 주세요..."
+                Title = "오랜만에 송년회 어떠세요?",
+                Description = "참석하고 싶은 마음이 불처럼 타오르는 분들 계신가요!!!!"
+            };
+
+            _predEngine = _mlContext.Model.CreatePredictionEngine<GitHubIssue, IssuePrediction>(loadedModel);
+            var prediction2 = _predEngine.Predict(singleIssue);
+            Console.WriteLine($"=============== Single Prediction - Result: {prediction2.Area} ===============");
+        }
+    }
+}
---
+++ SentimentAnalysis_Korean/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace SentimentAnalysis
+{
+    class Program
+    {
+        private static string _appPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        static readonly string _dataPath = Path.Combine(_appPath, "..", "..", "..", "Data", "naver_labelled.txt");
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+
+            // Load Data
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            // BuildAndTrainModel
+            var estimator = mlContext.Transforms.Text
+                .FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features", maximumNumberOfIterations: 100));
+
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(splitDataView.TrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+
+            // Evaluate            
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            IDataView predictions = model.Transform(splitDataView.TestSet);
+
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+
+            // UseModelWithSingleItem
+            PredictionEngine<SentimentData, SentimentPrediction> predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+
+            SentimentData sampleStatement = new SentimentData
+            {
+                SentimentText = "이 영화 정말 재미없어요"
+            };
+
+            var resultPrediction = predictionFunction.Predict(sampleStatement);
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset===============");
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
+
+            // UseModelWithBatchItems
+            IEnumerable<SentimentData> sentiments = new[]
+            {
+                new SentimentData { SentimentText = "지루한 영화에요" },
+                new SentimentData { SentimentText = "이거 정말 최고에요!" },
+                new SentimentData { SentimentText = "올해의 영화로 손꼽고 싶군요" }
+            };
+
+            IDataView batchComments = mlContext.Data.LoadFromEnumerable(sentiments);
+            predictions = model.Transform(batchComments);
+
+            // Use model to predict whether comment data is Positive (1) or Negative (0).
+            IEnumerable<SentimentPrediction> predictedResults = mlContext.Data.CreateEnumerable<SentimentPrediction>(predictions, reuseRowObject: false);
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of loaded model with multiple samples ===============");
+            foreach (SentimentPrediction prediction in predictedResults)
+            {
+                Console.WriteLine($"Sentiment: {prediction.SentimentText} | Prediction:{ (Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative")} | Probability:{ prediction.Probability}");
+            }
+            Console.WriteLine("=============== End of predictions ===============");
+        }
+    }
+}
